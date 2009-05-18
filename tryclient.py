40d7ec7e8d961ce66407aaa4ad85ca493732febd
elzouebi <elzouebi@y011pc99.einet.ad.eivd.ch>
2009-05-18 15:14:46 +0200

    add ffmeg control

---
+++ tryclient.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import dbus
+
+service = "ch.cett.misse.ffmpeg"
+ffmpeg = dbus.Interface(dbus.SessionBus().get_object(service,"/"), service)
+print 'Available commands are: \n'
+print '1- start \n'
+print '2- stop \n'
+print '3- image_size_set \n'
+print '4- image_size_get \n'
+print '5- image_rate_set \n'
+print '6- image_rate_get \n'
+print '7- bit_rate_set \n'
+print '8- bit_rate_get \n'
+print '9- client_ip_set \n'
+print '10- client_ip_get \n'
+
+while True:
+ x = int(raw_input("enter the number of the command: "))
+ if x < 1:
+      exit()
+ elif x == 1:
+      print ffmpeg.start()
+ elif x == 2:
+      ffmpeg.stop()
+ elif x == 3:
+      w = int(raw_input("the width of the frame is: "))
+      h = int(raw_input("the height of the frame is: "))
+      print ffmpeg.image_size_set(w, h)
+ elif x == 4:
+      print ffmpeg.image_size_get()
+ elif x == 5:
+      r= int(raw_input("the image rate is: "))
+      print ffmpeg.image_rate_set(r)
+ elif x == 6:
+      print ffmpeg.image_rate_get()
+ elif x == 7:
+      r = int(raw_input("the bit rate is: "))
+      print ffmpeg.bit_rate_set (r)
+ elif x == 8:
+      print ffmpeg.bit_rate_get()
+ elif x == 9:
+      s=raw_input("the ip is: ")
+      print ffmpeg.client_ip_set(s)
+ elif x == 10:
+      print ffmpeg.client_ip_get()
+
+ else:
+      print 'invalid'
+
+
---
+++ tryserver.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import subprocess
+import signal
+import gobject
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+import os
+
+service = "ch.cett.misse.ffmpeg"
+
+class FFMpeg(dbus.service.Object):
+    width=160
+    height=128
+    imrate=25
+    bitrate=200
+    @dbus.service.method(service, in_signature='', out_signature='i')
+    def start(self):
+        print "Should start ffmpeg"
+	s='ffmpeg -f video4linux2 -s '+ str(self.width)+'x'+str(self.height)+' -r '+ str(self.imrate) +' -b '+str(self.bitrate) +' -i  /dev/video0 -vcodec mjpeg  -f avi out.avi'
+        self.p =subprocess.Popen(s, shell=True,stdout=subprocess.PIPE)
+        return 0
+
+    @dbus.service.method(service, in_signature='', out_signature='i')
+    def stop(self):
+        print "Should stop ffmpeg"
+        os.kill(self.p.pid, signal.SIGTERM)
+        return 0
+
+    @dbus.service.method(service, in_signature='ii', out_signature='i')
+    def image_size_set(self, width, height):
+        print "image_size_set: width=%d height=%d"%(width, height)
+	self.width = width
+	self.height = height
+        return 0
+
+    @dbus.service.method(service, in_signature='', out_signature='ii')
+    def image_size_get(self):
+        print "image_size_get:"
+	return (self.width, self.height)
+
+
+    @dbus.service.method(service, in_signature='i', out_signature='i')
+    def image_rate_set(self,imrate):
+        print "image_rate_set: %d"%imrate
+	self.imrate=imrate
+	return 0
+
+    @dbus.service.method(service, in_signature='', out_signature='i')
+    def image_rate_get(self):
+        print "image_rate_get:"
+	return (self.imrate)
+
+    @dbus.service.method(service, in_signature='i', out_signature='i')
+    def bit_rate_set(self,bitrate):
+        print "bit_rate_set: %d"%bitrate
+	self.bitrate=bitrate
+	return 0
+
+
+
+    @dbus.service.method(service, in_signature='', out_signature='i')
+    def bit_rate_get(self):
+        print "Bit_rate_get:"
+	return (self.bitrate)
+
+  
+
+    @dbus.service.method(service, in_signature='s', out_signature='i')
+    def client_ip_set(self,clip):
+        print "client_ip_set:"
+        self.clip=clip
+	return 0
+
+
+    @dbus.service.method(service, in_signature='', out_signature='s')
+    def client_ip_get(self):
+        print "client_ip_get:"
+	return (self.clip)
+
+
+
+if __name__ == '__main__':
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    bus = dbus.SessionBus()
+    name = dbus.service.BusName(service, bus)
+    object = FFMpeg(bus, '/')
+    mainloop = gobject.MainLoop()
+    mainloop.run()
+

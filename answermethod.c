e6c1be791c98d79f301996984d11cc6eec552c28
elzouebi <elzouebi@y011pc99.einet.ad.eivd.ch>
2009-05-26 14:32:57 +0200

    how to read the arguments of function

--- answermethod.c
+++ answermethod.c
@@ -1,23 +1,88 @@
-/*
-* Example low-level D-Bus code.
-* Written by Matthew Johnson <dbus@matthew.ath.cx>
-*
-* This code has been released into the Public Domain.
-* You may do whatever you like with it.
-*/
 #include <dbus/dbus.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+uint level = 21614,width=160,height=128
+    ,imrate=25
+    ,bitrate=200
+    ,qscale=6,ok=0;
+char* clip="127.0.0.1";
 
 
-void image_rate_get(DBusMessage* msg, DBusConnection* conn)
+
+
+void start(DBusMessage* msg,DBusConnection* conn)
 {
+printf("video started \n");
    DBusMessage* reply;
    DBusMessageIter args;
-   dbus_uint32_t level = 21614;
+   dbus_uint32_t serial = 0;
+
+   // create a reply from the message
+   reply = dbus_message_new_method_return(msg);
+
+   // add the arguments to the reply
+   dbus_message_iter_init_append(reply, &args);
+   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_UINT32, &ok)) {
+      fprintf(stderr, "Out Of Memory!\n");
+      exit(1);
+   }
+   // send the reply && flush the connection
+   if (!dbus_connection_send(conn, reply, &serial)) {
+      fprintf(stderr, "Out Of Memory!\n");
+      exit(1);
+   }
+   dbus_connection_flush(conn);
+
+   // free the reply
+   dbus_message_unref(reply);
+}
+
+
+
+void image_size_set(DBusMessage* msg,DBusConnection* conn)
+{  
+
+   DBusMessage* reply;
+   DBusMessageIter args;
+   dbus_uint32_t serial = 0;
+   // read the arguments
+   if (!dbus_message_iter_init(msg, &args))
+      fprintf(stderr, "Message has no arguments!\n");
+   else if (DBUS_TYPE_UINT32 != dbus_message_iter_get_arg_type(&args))
+      fprintf(stderr, "Argument is not integer!\n");
+
+  printf("image size set\n");
+
+
+   // create a reply from the message
+   reply = dbus_message_new_method_return(msg);
+
+   // add the arguments to the reply
+   dbus_message_iter_init_append(reply, &args);
+   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_UINT32, &ok)) {
+      fprintf(stderr, "Out Of Memory!\n");
+      exit(1);
+   }
+   // send the reply && flush the connection
+   if (!dbus_connection_send(conn, reply, &serial)) {
+      fprintf(stderr, "Out Of Memory!\n");
+      exit(1);
+   }
+   dbus_connection_flush(conn);
+
+   // free the reply
+   dbus_message_unref(reply);
+}
+
+
+void image_rate_get(DBusMessage* msg,DBusConnection* conn)
+{
+
+   DBusMessage* reply;
+   DBusMessageIter args;
    dbus_uint32_t serial = 0;
    char* param = "";
 
@@ -36,7 +101,11 @@ void image_rate_get(DBusMessage* msg, DBusConnection* conn)
 
    // add the arguments to the reply
    dbus_message_iter_init_append(reply, &args);
-   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_UINT32, &level)) {
+   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_UINT32, &width)) {
+      fprintf(stderr, "Out Of Memory!\n");
+      exit(1);
+   }
+   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_UINT32, &height)) {
       fprintf(stderr, "Out Of Memory!\n");
       exit(1);
    }
@@ -58,10 +127,11 @@ void image_rate_get(DBusMessage* msg, DBusConnection* conn)
 void listen()
 {
    DBusMessage* msg;
+   DBusMessage* reply;
    DBusConnection* conn;
    DBusError err;
    int ret;
-
+   dbus_uint32_t serial = 0;
    printf("Listening for method calls\n");
 
    // initialise the error
@@ -91,7 +161,8 @@ DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
    }
 
    // loop, testing for new messages
-   while (true) {
+   while (true)
+   {
       // non blocking read of the next available message
       dbus_connection_read_write(conn, 0);
       msg = dbus_connection_pop_message(conn);
@@ -104,25 +175,64 @@ DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
 
       // check this is a method call for the right interface & method
 
-      if (dbus_message_is_method_call(msg, "org.freedesktop.DBus.Introspectable", "Introspect")) {
-      printf("must do introspection \n");
-       static const char introspect_xml[] =
-  "<!DOCTYPE node PUBLIC \"-//freedesktop//DTD D-BUS Object Introspection 1.0//EN\"\n"
-  "                      \"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd\">\n"
-  "<node>\n"
-  "  <interface name=\"ch.cett.misse.ffmpeg\">\n"
-  "    <method name=\"image_rate_get\">\n"
-  "      <arg name=\"port\" direction=\"out\" type=\"u\"/>\n"
-  "    </method>\n"
-  "  </interface>\n"
-  "</node>\n";
-                                                                                                }
-      if (dbus_message_is_method_call(msg, "ch.cett.misse.ffmpeg", "image_rate_get")) {
-         image_rate_get(msg, conn);
-      } else {
-	      fprintf(stderr, "Unknown message: member=%s\n", dbus_message_get_member(msg));
-      }
+      if (dbus_message_is_method_call(msg, "org.freedesktop.DBus.Introspectable", "Introspect")) 
+	{
+		printf("must do introspection \n");
+	         static const char* introspect_xml =
+	  	"<!DOCTYPE node PUBLIC \"-//freedesktop//DTD D-BUS Object Introspection 1.0//EN\"\n"
+	 	 "                      \"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd\">\n"
+		  "<node>\n"
+		  "  <interface name=\"ch.cett.misse.ffmpeg\">\n"
+	  	"    <method name=\"image_rate_get\">\n"
+	  	"      <arg name=\"port\" direction=\"out\" type=\"uu\"/>\n"
+	  	"    </method>\n"
+	  	"    <method name=\"start\">\n"
+	  //	"      <arg name=\"port\" direction=\"out\" type=\"uu\"/>\n"
+	  	"    </method>\n"
+	  	"    <method name=\"image_rate_set\">\n"
+                "    <arg name=\"width\"  direction=\"in\" type=\"u\" /> \n"
+                "    <arg name=\"height\"  direction=\"in\" type=\"u\" /> \n"
+	  	"    <arg name=\"port\" direction=\"out\" type=\"u\"/>\n"
+	  	"    </method>\n"
 
+	 	 "  </interface>\n"
+	 	 "</node>\n";
+		reply = dbus_message_new_method_return(msg);
+               if (!dbus_message_append_args (reply,
+                                 DBUS_TYPE_STRING, &introspect_xml,
+                                 DBUS_TYPE_INVALID))
+		{
+		      fprintf(stderr, "Out Of Memory!\n");
+		      exit(1);
+		}
+
+		if (!dbus_connection_send(conn, reply, &serial)) 
+		{
+      		fprintf(stderr, "Out Of Memory!\n");
+      		exit(1);
+   		}
+   		dbus_connection_flush(conn);
+
+		dbus_message_unref(reply);
+	}
+      if (dbus_message_is_method_call(msg, "ch.cett.misse.ffmpeg", "image_rate_get")) 
+	{
+         image_rate_get(msg, conn);
+	} 
+     if (dbus_message_is_method_call(msg, "ch.cett.misse.ffmpeg", "start")) 
+	{
+	 start(msg,conn);
+	}
+     if (dbus_message_is_method_call(msg, "ch.cett.misse.ffmpeg", "image_size_set")) 
+	{
+	 image_size_set(msg,conn);
+	}
+/*
+	else 
+	 {
+	      fprintf(stderr, "Unknown message: member=%s\n", dbus_message_get_member(msg));
+         }
+*/
       // free the message
       dbus_message_unref(msg);
    }

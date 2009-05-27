8302862e76758cb2882c478cf3db077a4784e76a
elzouebi <elzouebi@y011pc99.einet.ad.eivd.ch>
2009-05-27 16:28:02 +0200

    error execvp with ffmpeg

--- callmethod.c
+++ callmethod.c
@@ -1,129 +1,21 @@
-/*
-* Example low-level D-Bus code.
-* Written by Matthew Johnson <dbus@matthew.ath.cx>
-*
-* This code has been released into the Public Domain.
-* You may do whatever you like with it.
-*/
-#include <dbus/dbus.h>
-#include <stdbool.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
-* Connect to the DBUS bus and send a broadcast signal
-*/
-
-void query(char* param)
-{
-   DBusMessage* msg;
-   DBusMessageIter args;
-   DBusConnection* conn;
-   DBusError err;
-   DBusPendingCall* pending;
-   int ret;
-   bool stat;
-   dbus_uint32_t level;
-
-   printf("Calling remote method with %s\n", param);
-
-   // initialiset the errors
-   dbus_error_init(&err);
-
-   // connect to the system bus and check for errors
-   conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
-   if (dbus_error_is_set(&err)) {
-      fprintf(stderr, "Connection Error (%s)\n", err.message);
-      dbus_error_free(&err);
-   }
-   if (NULL == conn) {
-      exit(1);
-   }
-
-   // request our name on the bus
-   ret = dbus_bus_request_name(conn, "ch.cett.misse.ffmpeg",
-			       DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
-   if (dbus_error_is_set(&err)) {
-      fprintf(stderr, "Name Error (%s)\n", err.message);
-      dbus_error_free(&err);
-   }
-   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) {
-      exit(1);
-   }
-
-   // create a new method call and check for errors
-   msg = dbus_message_new_method_call("ch.cett.misse.ffmpeg", // target for the method call
-                                      "/", // object to call on
-                                      "ch.cett.misse.ffmpeg", // interface to call on
-                                      "image_rate_get"); // method name
-   if (NULL == msg) {
-      fprintf(stderr, "Message Null\n");
-      exit(1);
-   }
-//test.method.server
-   // append arguments
-   dbus_message_iter_init_append(msg, &args);
-   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &param)) {
-      fprintf(stderr, "Out Of Memory!\n");
-      exit(1);
-   }
-
-   // send message and get a handle for a reply
-   if (!dbus_connection_send_with_reply (conn, msg, &pending, -1)) {
-// -1 is default timeout
-      fprintf(stderr, "Out Of Memory!\n");
-      exit(1);
-   }
-   if (NULL == pending) {
-      fprintf(stderr, "Pending Call Null\n");
-      exit(1);
-   }
-   dbus_connection_flush(conn);
-
-   printf("Request Sent\n");
-
-   // free message
-   dbus_message_unref(msg);
-
-   // block until we recieve a reply
-   dbus_pending_call_block(pending);
-
-   // get the reply message
-   msg = dbus_pending_call_steal_reply(pending);
-   if (NULL == msg) {
-      fprintf(stderr, "Reply Null\n");
-      exit(1);
-   }
-   // free the pending message handle
-   dbus_pending_call_unref(pending);
-
-   // read the parameters
-   if (!dbus_message_iter_init(msg, &args))
-      fprintf(stderr, "Message has no arguments!\n");
-   else if (DBUS_TYPE_BOOLEAN != dbus_message_iter_get_arg_type(&args))
-      fprintf(stderr, "Argument is not boolean!\n");
-   else
-      dbus_message_iter_get_basic(&args, &stat);
-
-   if (!dbus_message_iter_next(&args))
-      fprintf(stderr, "Message has too few arguments!\n");
-   else if (DBUS_TYPE_UINT32 != dbus_message_iter_get_arg_type(&args))
-      fprintf(stderr, "Argument is not int!\n");
-   else
-      dbus_message_iter_get_basic(&args, &level);
-
-   printf("Got Reply: %d, %d\n", stat, level);
-
-   // free reply
-   dbus_message_unref(msg);
-}
-
-
 int main()
-{
-   char* param = "no param";
-      query(param);
 
-   return 0;
+{
+   pid_t pid;
+printf("/dev/video0 \n");
+ char *const parmList[] = 
+   {"/usr/bin/ffmpeg", "-f","video4linux2 ","-s","640x480 ","-r "," 15"," -i", " /dev/video0 ", "-f", "mjpeg","udp:127.0.0.1:1234",  NULL}; 
+
+   if ((pid = fork()) == -1)
+      perror("fork() error");
+   else if (pid == 0) {
+      execvp("ffmpeg", parmList);
+
+return 0;
+   }
 }

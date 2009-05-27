adc56fe1d2c9793efb2eb0fe9d05b1beffc471c8
Jean-Christian de Rivaz <jc@eclis.ch>
2009-05-27 16:40:56 +0200

    Remove buggy spaces

--- callmethod.c
+++ callmethod.c
@@ -9,7 +9,7 @@ int main()
    pid_t pid;
 printf("/dev/video0 \n");
  char *const parmList[] = 
-   {"/usr/bin/ffmpeg", "-f","video4linux2 ","-s","640x480 ","-r "," 15"," -i", " /dev/video0 ", "-f", "mjpeg","udp:127.0.0.1:1234",  NULL}; 
+   {"ffmpeg", "-f","video4linux2","-s","640x480 ","-r"," 15","-i", "/dev/video0", "-f", "mjpeg","udp:127.0.0.1:1234",  NULL}; 
 
    if ((pid = fork()) == -1)
       perror("fork() error");

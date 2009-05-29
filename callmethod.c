9ca23645cb51e043e4aab61e71ea0eeaf0e8f048
elzouebi <elzouebi@y011pc99.einet.ad.eivd.ch>
2009-05-29 11:50:42 +0200

    it's ok with ls, but failed with ffmpeg

--- callmethod.c
+++ callmethod.c
@@ -1,21 +1,55 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+  #include <stdio.h>
+  #include <memory.h>
+  #include <unistd.h>
 
-int main()
+  int main( int argc, char ** argv )
+  {
 
-{
-   pid_t pid;
-printf("/dev/video0 \n");
- char *const parmList[] = 
-   {"ffmpeg", "-f","video4linux2","-s","640x480 ","-r"," 15","-i", "/dev/video0", "-f", "mjpeg","udp:127.0.0.1:1234",  NULL}; 
+  
+   /* create the pipe */
+   int pfd[2];
+     char buffer[BUFSIZ+1];
 
-   if ((pid = fork()) == -1)
-      perror("fork() error");
-   else if (pid == 0) {
-      execvp("ffmpeg", parmList);
+char * parmList[]= //{"ls","-l",NULL};
+ {"/usr/bin/ffmpeg", "-f","video4linux2","-s","640x480","-r","15","-i","/dev/video0","-vcodec","mjpeg","-f","avi","out.avi",NULL}; 
+   if (pipe(pfd) == -1)
+     {
+       printf("pipe failed\n");
+       return 1;
+     }
 
-return 0;
-   }
-}
+   /* create the child */
+   int pid;
+   if ((pid = fork()) < 0)
+     {
+       printf("fork failed\n");
+       return 2;
+     }
+
+   if (pid == 0)
+     {
+       /* child */
+       close(pfd[0]); /* close the unused read side */
+       dup2(pfd[1], 1); /* connect the write side with stdout */
+       close(pfd[1]); /* close the write side */
+       /* execute the process (wc command) */
+       execvp("ffmpeg", parmList);
+       printf("ls failed"); /* if execlp returns, it's an error */
+       return 3;
+     }
+   else
+     {
+       /* parent */
+       close(pfd[1]); /* close the unused write side */
+       while (read(pfd[0], buffer, BUFSIZ) != 0)
+      {
+         printf("parent reads ");
+   
+      }
+       close(pfd[0]); /* close the read side */
+       /* execute the process (ls command) */
+ 
+       return 4;
+     }
+   return 0;
+  }
